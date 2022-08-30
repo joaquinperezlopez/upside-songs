f1b827c0bc6b86812dcf0583b6b464576a35a701
Joaquín Pérez <joaquin@adaviv.com>
2022-08-30 23:13:01 +0200

    Unifying type files

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,9 @@
 declare module 'react-native-base64' {
   export const encode: (input: string) => string;
 }
+
+declare module 'react-native-config/Config' {
+  export const WS_USERNAME: string;
+  export const WS_PASSWORD: string;
+  export const WS_URL: string;
+}
--- react-native-config.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'react-native-config/Config' {
-  export const WS_USERNAME: string;
-  export const WS_PASSWORD: string;
-  export const WS_URL: string;
-}
